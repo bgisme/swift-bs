93a3c72c0d61234421727bc863cc288b9e00861a
bgisme <bg4all@me.com>
2022-02-20 09:48:47 -0500

    Implemented buildArray(...)

--- Sources/SwiftBs/Extensions/TagBuilder+TagRepresentable.swift
+++ Sources/SwiftBs/Extensions/TagBuilder+TagRepresentable.swift
@@ -16,6 +16,10 @@ extension TagBuilder {
     static func buildExpression(_ expression: [TagRepresentable]) -> [Tag] {
         expression.map { $0.build() }
     }
+    
+    static func buildArray(_ components: [TagRepresentable]) -> [Tag] {
+        components.compactMap{ $0 }.map{ $0.build() }
+    }
 }
 
 public protocol TagRepresentable {
