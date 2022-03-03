1d1030a78bffb25b392a9d5598e8ab22f5de7e49
bgisme <bg4all@me.com>
2022-03-03 13:01:38 -0500

    minor

--- Sources/SwiftBs/Components/Dropdown.swift
+++ Sources/SwiftBs/Components/Dropdown.swift
@@ -74,7 +74,7 @@ extension Dropdown: TagRepresentable {
     
     @TagBuilder
     public func build() -> Tag {
-        if direction != .start {
+        if isSplit && direction != .start {
             Div {
                 if isSplit {
                     /// split buttons direction .start are ordered differently
