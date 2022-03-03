08e982c52597665c851d3ed43d06512a8e1dbd44
bgisme <bg4all@me.com>
2022-03-03 13:31:31 -0500

    minor

--- Sources/SwiftBs/Components/Dropdown.swift
+++ Sources/SwiftBs/Components/Dropdown.swift
@@ -33,9 +33,9 @@ public class Dropdown: Component {
     public typealias Id = String
     public typealias IsSplit = Bool
     
-    private let id: String
-    private let direction: Direction
-    private let isSplit: Bool
+    let id: String
+    let direction: Direction
+    let isSplit: Bool
     private var button: (Id, IsSplit, Dropdown.Direction) -> [Tag]
     private var splitButton: (Id) -> [Tag]
     private var menu: (Id) -> [Tag]
@@ -111,10 +111,10 @@ public class DropdownButton: Component {
         case link(_ title: String?, href: String)
     }
     
-    private let type: `Type`
-    private let id: String
-    private let direction: Dropdown.Direction
-    private let isSplit: Bool
+    let type: `Type`
+    let id: String
+    let direction: Dropdown.Direction
+    let isSplit: Bool
     
     public init(type: `Type`,
                 id: String,
@@ -190,7 +190,7 @@ extension DropdownButton: TagRepresentable {
 
 public class DropdownButtonArrow: Component {
     
-    private let id: String
+    let id: String
     
     public init(id: String) {
         self.id = id
@@ -245,17 +245,23 @@ extension DropdownMenu: TagRepresentable {
 
 public class DropdownItem: Component {
     
-    let a: A
+    let tag: Tag
     let isActive: Bool
     let isDisabled: Bool
-    
-    public convenience init(_ title: String, href: String, isActive: Bool = false, isDisabled: Bool = false) {
-        let a = A(title).href(href)
-        self.init(a: a, isActive: isActive, isDisabled: isDisabled) {}
+        
+    public convenience init(_ a: A, isActive: Bool = false, isDisabled: Bool = false, @TagBuilder children: @escaping () -> [Tag]) {
+        self.init(tag: a, isActive: isActive, isDisabled: isDisabled, children: children)
     }
     
-    public init(a: A, isActive: Bool = false, isDisabled: Bool = false, @TagBuilder children: @escaping () -> [Tag]) {
-        self.a = a
+    public convenience init(_ button: Button,
+                            isActive: Bool = false,
+                            isDisabled: Bool = false,
+                            @TagBuilder children: @escaping () -> [Tag]) {
+        self.init(tag: button, isActive: isActive, isDisabled: isDisabled, children: children)
+    }
+    
+    private init(tag: Tag, isActive: Bool = false, isDisabled: Bool = false, @TagBuilder children: @escaping () -> [Tag]) {
+        self.tag = tag
         self.isActive = isActive
         self.isDisabled = isDisabled
         super.init(children)
@@ -267,7 +273,7 @@ extension DropdownItem: TagRepresentable {
     @TagBuilder
     public func build() -> Tag {
         Li {
-            a
+            tag
                 .class(.dropdownItem)
                 .class(add: .active, if: isActive)
                 .class(add: .disabled, if: isDisabled)
