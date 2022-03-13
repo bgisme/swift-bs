edde0bc7517af4c62bbdc6f284d31cd4a9bf9871
bgisme <bg4all@me.com>
2022-03-13 16:01:20 -0400

    minor

--- Sources/SwiftBs/Components/Collapse.swift
+++ Sources/SwiftBs/Components/Collapse.swift
@@ -68,7 +68,7 @@ public class CollapseButton: Component {
     let ids: [String]
     
     public convenience init(_ title: String, contentId id: String) {
-        self.init(title, [id])
+        self.init(title, contentIds: [id])
     }
     
     public convenience init(_ title: String, contentIds ids: [String]) {
