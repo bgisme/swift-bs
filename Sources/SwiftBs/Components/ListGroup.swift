c3eecb66d7853a0fba9ddd1a74cb839679027706
bgisme <bg4all@me.com>
2022-03-14 19:03:37 -0400

    minor

--- Sources/SwiftBs/Components/ListGroup.swift
+++ Sources/SwiftBs/Components/ListGroup.swift
@@ -50,43 +50,48 @@ extension ListGroup: TagRepresentable {
 public class ListGroupItem: Component {
     
     let tag: Tag
+    let isAction: Bool
     let isActive: Bool
     let isDisabled: Bool
     
     public convenience init(_ text: String,
                             isActive: Bool = false,
                             isDisabled: Bool = false) {
-        self.init(tag: Li(text), isActive: isActive, isDisabled: isDisabled)
+        self.init(tag: Li(text), isAction: false, isActive: isActive, isDisabled: isDisabled)
     }
     
     public convenience init(_ li: Li,
                             isActive: Bool = false,
                             isDisabled: Bool = false) {
-        self.init(tag: li, isActive: isActive, isDisabled: isDisabled)
+        self.init(tag: li, isAction: false, isActive: isActive, isDisabled: isDisabled)
     }
     
+    /// <a> do not support disabled
     public convenience init(_ a: A,
-                            isActive: Bool = false,
-                            isDisabled: Bool = false) {
-        self.init(tag: a, isActive: isActive, isDisabled: isDisabled)
+                            isAction: Bool = false,
+                            isActive: Bool = false) {
+        self.init(tag: a, isAction: isAction, isActive: isActive, isDisabled: false)
     }
     
     public convenience init(_ button: Button,
+                            isAction: Bool = false,
                             isActive: Bool = false,
                             isDisabled: Bool = false) {
-        self.init(tag: button, isActive: isActive, isDisabled: isDisabled)
+        self.init(tag: button, isAction: isAction, isActive: isActive, isDisabled: isDisabled)
     }
     
     public convenience init(_ label: Label,
                             isActive: Bool = false,
                             isDisabled: Bool = false) {
-        self.init(tag: label, isActive: isActive, isDisabled: isDisabled)
+        self.init(tag: label, isAction: false, isActive: isActive, isDisabled: isDisabled)
     }
     
     internal init(tag: Tag,
+                  isAction: Bool,
                   isActive: Bool,
                   isDisabled: Bool) {
         self.tag = tag
+        self.isAction = isAction
         self.isActive = isActive
         self.isDisabled = isDisabled
     }
@@ -98,27 +103,10 @@ extension ListGroupItem: TagRepresentable {
     public func build() -> Tag {
         tag
             .class(add: .listGroupItem)
+            .class(add: .listGroupItemAction, if: isAction)
             .class(add: .active, if: isActive)
             .ariaCurrent(isActive)
             .class(add: .disabled, if: isDisabled)
             .ariaDisabled(isDisabled)
-    }
-}
-
-public class ListGroupActionItem: Component {
-    
-    let tag: Tag
-    
-    internal init(tag: Tag) {
-        self.tag = tag
-    }
-}
-
-extension ListGroupActionItem: TagRepresentable {
-    
-    @TagBuilder
-    public func build() -> Tag {
-        tag
-            .class(add: .listGroupItem, .listGroupItemAction)
     }
 }
