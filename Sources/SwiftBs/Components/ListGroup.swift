4ca0a30eeb0233f55a472bff06009e2934c4592a
bgisme <bg4all@me.com>
2022-03-14 17:12:33 -0400

    minor

---
+++ Sources/SwiftBs/Components/ListGroup.swift
@@ -0,0 +1,126 @@
+import SwiftHtml
+import SwiftSgml
+
+public class ListGroup: Component {
+    
+    let tag: Tag
+    let isFlush: Bool
+    let isNumbered: Bool
+    let isHorizontal: Bool
+    
+    public convenience init(unordered items: ListGroupItem...,
+                            isFlush: Bool = false,
+                            isNumbered: Bool = false,
+                            isHorizontal: Bool = false) {
+        self.init(unordered: items, isFlush: isFlush, isNumbered: isNumbered, isHorizontal: isHorizontal)
+    }
+    
+    public convenience init(unordered items: [ListGroupItem],
+                            isFlush: Bool = false,
+                            isNumbered: Bool = false,
+                            isHorizontal: Bool = false) {
+        self.init(tag: Ul { items.map{$0} }, isFlush: isFlush, isNumbered: isNumbered, isHorizontal: isHorizontal)
+    }
+    
+    public convenience init(ordered items: ListGroupItem...,
+                            isFlush: Bool = false,
+                            isNumbered: Bool = false,
+                            isHorizontal: Bool = false) {
+        self.init(ordered: items, isFlush: isFlush, isNumbered: isNumbered, isHorizontal: isHorizontal)
+    }
+    
+    public convenience init(ordered items: [ListGroupItem],
+                            isFlush: Bool = false,
+                            isNumbered: Bool = false,
+                            isHorizontal: Bool = false) {
+        self.init(tag: Ol { items.map{$0} }, isFlush: isFlush, isNumbered: isNumbered, isHorizontal: isHorizontal)
+    }
+    
+    public convenience init(action items: ListGroupActionItem...,
+                            isFlush: Bool = false,
+                            isNumbered: Bool = false,
+                            isHorizontal: Bool = false) {
+        self.init(action: items, isFlush: isFlush, isNumbered: isNumbered, isHorizontal: isHorizontal)
+    }
+    
+    public convenience init(action items: [ListGroupActionItem],
+                            isFlush: Bool = false,
+                            isNumbered: Bool = false,
+                            isHorizontal: Bool = false) {
+        self.init(tag: Div { items.map{$0} }, isFlush: isFlush, isNumbered: isNumbered, isHorizontal: isHorizontal)
+    }
+
+    internal init(tag: Tag,
+                  isFlush: Bool,
+                  isNumbered: Bool,
+                  isHorizontal: Bool) {
+        self.tag = tag
+        self.isFlush = isFlush
+        self.isNumbered = isNumbered
+        self.isHorizontal = isHorizontal
+    }
+}
+
+extension ListGroup: TagRepresentable {
+    
+    @TagBuilder
+    public func build() -> Tag {
+        tag
+            .class(add: .listGroup)
+            .class(add: .listGroupFlush, if: isFlush)
+            .class(add: .listGroupNumbered, if: isNumbered)
+            .class(add: .listGroupHorizontal, if: isHorizontal)
+    }
+}
+
+public class ListGroupItem: Component {
+    
+    let tag: Tag
+    
+    public convenience init(_ li: Li) {
+        self.init(tag: li)
+    }
+    
+    public convenience init(_ a: A) {
+        self.init(tag: a)
+    }
+    
+    public convenience init(_ button: Button) {
+        self.init(tag: button)
+    }
+    
+    public convenience init(_ label: Label) {
+        self.init(tag: label)
+    }
+    
+    internal init(tag: Tag) {
+        self.tag = tag
+    }
+}
+
+extension ListGroupItem: TagRepresentable {
+    
+    @TagBuilder
+    public func build() -> Tag {
+        tag
+            .class(add: .listGroupItem)
+    }
+}
+
+public class ListGroupActionItem: Component {
+    
+    let tag: Tag
+    
+    internal init(tag: Tag) {
+        self.tag = tag
+    }
+}
+
+extension ListGroupActionItem: TagRepresentable {
+    
+    @TagBuilder
+    public func build() -> Tag {
+        tag
+            .class(add: .listGroupItem, .listGroupItemAction)
+    }
+}
---
+++ Tests/SwiftBsTests/ListGroupTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftHtml
+@testable import SwiftBs
+
+final class ListGroupTests: XCTestCase {
+    
+    func testListGroup() throws {
+        //! IMPLEMENT
+        XCTFail()
+    }
+}
