a52f84e6db40b04a6d7988a3bd133a42df5e9a2c
bgisme <bg4all@me.com>
2022-03-19 15:02:24 -0400

    minor

---
+++ Sources/SwiftBs/Enums/Bootstrapable.swift
@@ -0,0 +1,17 @@
+//
+//  Bootstrapable.swift
+//  
+//
+//  Created by Brad Gourley on 3/19/22.
+//
+
+public protocol Bootstrapable {
+    
+    func `class`(insert classes: BsClass..., if condition: Bool) -> Self
+    
+    func `class`(insert classes: [BsClass], _ condition: Bool) -> Self
+    
+    func style(set styles: CssKeyValue..., if condition: Bool) -> Self
+    
+    func style(set styles: [CssKeyValue], _ condition: Bool) -> Self
+}
