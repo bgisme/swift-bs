21550d70fec703fbee4ba3c6e99bd49768bb62e6
bgisme <bg4all@me.com>
2022-03-19 20:47:05 -0400

    minor

--- Sources/SwiftBs/Components/Alert.swift
+++ Sources/SwiftBs/Components/Alert.swift
@@ -12,17 +12,25 @@ public class Alert: Component {
         
     /// children ... Img, AlertHeading, Any
     /// use isAlignCenter to align Img and other children
-    public init(isAlignedCenter: Bool = false, div: () -> Div) {
+    public init(isAlignedCenter: Bool = false, @TagBuilder contents: () -> [Tag]) {
         super.init {
-            div()
-                .class(insert: .alert)
-                .class(insert: .dFlex, .alignItemsCenter, if: isAlignedCenter)
-                .role(.alert)
+            Div {
+                contents()
+            }
+            .class(insert: .alert)
+            .class(insert: .dFlex, .alignItemsCenter, if: isAlignedCenter)
+            .role(.alert)
         }
     }
 }
 
 public class AlertHeading: Component {
+    
+    public convenience init(_ text: String) {
+        self.init {
+            H4(text)
+        }
+    }
     
     public init(_ h4: () -> H4) {
         super.init {
