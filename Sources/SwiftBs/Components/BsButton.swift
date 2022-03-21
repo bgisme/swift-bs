534cca3c12378dea8a1a51f31934d6b9430b2a70
bgisme <bg4all@me.com>
2022-03-21 09:33:55 -0400

    minor

--- Sources/SwiftBs/Components/BsButton.swift
+++ Sources/SwiftBs/Components/BsButton.swift
@@ -9,12 +9,13 @@ import SwiftHtml
 
 public class BsButton: Component {
     
-    public convenience init(isToggle: Bool = false,
+    public convenience init(color: Color? = nil,
+                            isToggle: Bool = false,
                             isPressed: Bool = false,
                             isDisabled: Bool = false,
                             isActive: Bool = false,
                             button: () -> Button) {
-        self.init {
+        self.init(color: color) {
             button()
                 .type(.button)
                 .class(insert: .active, if: isToggle && isPressed)
@@ -25,10 +26,11 @@ public class BsButton: Component {
         }
     }
     
-    public convenience init(isDisabled: Bool = false,
+    public convenience init(color: Color? = nil,
+                            isDisabled: Bool = false,
                             isActive: Bool = false,
                             a: () -> A) {
-        self.init {
+        self.init(color: color) {
             a()
                 .class(insert: .disabled, if: isDisabled)
                 .role(.button)
@@ -38,14 +40,38 @@ public class BsButton: Component {
         }
     }
     
-    public convenience init(input: () -> Input) {
-        self.init(tag: input)
+    public convenience init(color: Color? = nil, input: () -> Input) {
+        self.init(color: color, tag: input)
     }
     
-    internal init(tag: () -> Tag) {
+    internal init(color: Color?, tag: () -> Tag) {
+        let colorClass: BsClass?
+        if let color = color {
+            switch color {
+            case .primary:
+                colorClass = .btnPrimary
+            case .secondary:
+                colorClass = .btnSecondary
+            case .success:
+                colorClass = .btnSuccess
+            case .danger:
+                colorClass = .btnDanger
+            case .warning:
+                colorClass = .btnWarning
+            case .info:
+                colorClass = .btnInfo
+            case .light:
+                colorClass = .btnLight
+            case .dark:
+                colorClass = .btnDark
+            }
+        } else {
+            colorClass = nil
+        }
         super.init {
             tag()
                 .class(insert: .btn)
+                .class(insert: colorClass)
         }
     }
 }
