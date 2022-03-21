8099214aeee78e6d0dc2fded97dd1785cd8038d5
bgisme <bg4all@me.com>
2022-03-21 14:12:50 -0400

    minor

--- Sources/SwiftBs/Components/BsButton.swift
+++ Sources/SwiftBs/Components/BsButton.swift
@@ -9,14 +9,21 @@ import SwiftHtml
 
 public class BsButton: Component {
     
+    public enum Size {
+        case sm
+        case lg
+    }
+    
     public convenience init(color: Color? = nil,
                             isOutlined: Bool = false,
+                            size: Size? = nil,
+                            isBlockLevel: Bool = false,
                             isToggle: Bool = false,
                             isPressed: Bool = false,
                             isDisabled: Bool = false,
                             isActive: Bool = false,
                             button: () -> Button) {
-        self.init(color: color, isOutlined: isOutlined) {
+        self.init(color: color, isOutlined: isOutlined, size: size, isBlockLevel: isBlockLevel) {
             button()
                 .type(.button)
                 .class(insert: .active, if: isToggle && isPressed)
@@ -29,10 +36,12 @@ public class BsButton: Component {
     
     public convenience init(color: Color? = nil,
                             isOutlined: Bool = false,
+                            size: Size? = nil,
+                            isBlockLevel: Bool = false,
                             isDisabled: Bool = false,
                             isActive: Bool = false,
                             a: () -> A) {
-        self.init(color: color, isOutlined: isOutlined) {
+        self.init(color: color, isOutlined: isOutlined, size: size, isBlockLevel: isBlockLevel) {
             a()
                 .class(insert: .disabled, if: isDisabled)
                 .role(.button)
@@ -44,12 +53,16 @@ public class BsButton: Component {
     
     public convenience init(color: Color? = nil,
                             isOutlined: Bool = false,
+                            size: Size? = nil,
+                            isBlockLevel: Bool = false,
                             input: () -> Input) {
-        self.init(color: color, isOutlined: isOutlined, tag: input)
+        self.init(color: color, isOutlined: isOutlined, size: size, isBlockLevel: isBlockLevel, tag: input)
     }
     
     internal init(color: Color?,
                   isOutlined: Bool,
+                  size: Size? = nil,
+                  isBlockLevel: Bool = false,
                   tag: () -> Tag) {
         let colorClass: BsClass?
         if let color = color {
@@ -74,10 +87,23 @@ public class BsButton: Component {
         } else {
             colorClass = nil
         }
+        let sizeClass: BsClass?
+        if let size = size {
+            switch size {
+            case .sm:
+                sizeClass = .btnSm
+            case .lg:
+                sizeClass = .btnLg
+            }
+        } else {
+            sizeClass = nil
+        }
         super.init {
             tag()
                 .class(insert: .btn)
                 .class(insert: colorClass)
+                .class(insert: sizeClass)
+                .class(insert: .btnBlock, if: isBlockLevel)
         }
     }
 }
