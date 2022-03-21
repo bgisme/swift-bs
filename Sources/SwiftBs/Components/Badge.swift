687d3a6d9ab255e74ff5547d9282fe3bb686d4a0
bgisme <bg4all@me.com>
2022-03-21 09:26:01 -0400

    minor

--- Sources/SwiftBs/Components/Badge.swift
+++ Sources/SwiftBs/Components/Badge.swift
@@ -10,7 +10,7 @@ import SwiftHtml
 public class Badge: Component {
     
     public convenience init(_ text: String,
-                            color: BgColor,
+                            color: Color,
                             isPositioned: Bool = false,
                             isRounded: Bool = false) {
         self.init(color: color, isPositioned: isPositioned, isRounded: isRounded) {
@@ -18,14 +18,33 @@ public class Badge: Component {
         }
     }
     
-    public init(color: BgColor,
+    public init(color: Color,
                 isPositioned: Bool = false,
                 isRounded: Bool = false,
                 span: () -> Span) {
+        let colorClass: BsClass
+        switch color {
+        case .primary:
+            colorClass = .bgPrimary
+        case .secondary:
+            colorClass = .bgSecondary
+        case .success:
+            colorClass = .bgSuccess
+        case .danger:
+            colorClass = .bgDanger
+        case .warning:
+            colorClass = .bgWarning
+        case .info:
+            colorClass = .bgInfo
+        case .light:
+            colorClass = .bgLight
+        case .dark:
+            colorClass = .bgDark
+        }
         super.init {
             span()
                 .class(insert: .badge)
-                .class(insert: color.class)
+                .class(insert: colorClass)
                 .class(insert: .positionAbsolute, .top0, .start100, .translateMiddle, .roundedPill, if: isPositioned)
                 .class(insert: .roundedPill, if: isRounded)
         }
