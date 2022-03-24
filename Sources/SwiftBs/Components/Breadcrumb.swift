815f98da2b0e2ce16ac301d174c6f2dca2fea2f2
bgisme <bg4all@me.com>
2022-03-24 10:05:06 -0400

    minor

--- Sources/SwiftBs/Components/Breadcrumb.swift
+++ Sources/SwiftBs/Components/Breadcrumb.swift
@@ -21,15 +21,15 @@ public class Breadcrumb: Component {
     public static let breadcrumbArrow = "url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;)"
     public static let breadcrumbDividerRemoved = "''"
     
-    public convenience init(divider: String? = nil,
-                            @TagBuilder breadcrumbItems: () -> [Tag]) {
-        self.init(divider: divider, breadcrumbList: {
+    public static func withList(divider: String? = nil,
+                                @TagBuilder breadcrumbItems: () -> [Tag]) -> Breadcrumb {
+        Breadcrumb(divider: divider, breadcrumbList: {
             BreadcrumbList {
                 breadcrumbItems()
             }
         })
     }
-        
+    
     public convenience init(divider: String? = nil,
                             breadcrumbList: () -> BreadcrumbList) {
         let nav: Nav
