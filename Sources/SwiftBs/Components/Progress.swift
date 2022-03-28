f4cc6aabd202de6f0da55cc7ff8002e72222a43c
bgisme <bg4all@me.com>
2022-03-28 08:40:10 -0400

    minor

--- Sources/SwiftBs/Components/Progress.swift
+++ Sources/SwiftBs/Components/Progress.swift
@@ -9,25 +9,8 @@ import SwiftHtml
 
 public class Progress: Component {
     
-    public convenience init(percent: Int,
-                            label: String? = nil,
-                            height pixels: Int? = nil,
-                            theme: ColorTheme? = nil,
-                            isStriped: Bool = false,
-                            isAnimated: Bool = false) {
-        self.init {
-            Progressbar(percent: percent,
-                        label: label,
-                        height: pixels,
-                        theme: theme,
-                        isStriped: isStriped,
-                        isAnimated: isAnimated) {}
-        }
-    }
-    
-    /// contents ... Progressbar
-    public convenience init(@TagBuilder contents: () -> [Tag]) {
-        let div = Div { contents() }
+    public convenience init(progressBar: () -> Progressbar) {
+        let div = Div { progressBar() }
         self.init(div)
     }
     
@@ -41,63 +24,51 @@ public class Progress: Component {
 
 public class Progressbar: Component {
     
-    public convenience init(percent: Int,
-                            label: String? = nil,
-                            height pixels: Int? = nil,
-                            theme: ColorTheme? = nil,
-                            isStriped: Bool = false,
-                            isAnimated: Bool = false) {
-        self.init(percent: percent,
-                  label: label,
-                  height: pixels,
-                  theme: theme,
-                  isStriped: isStriped,
-                  isAnimated: isAnimated,
-                  contents: {})
-    }
-    
     /// percent > 100 becomes 100
-    /// contents ... anything (usually nothing)
-    public convenience init(percent: Int,
-                            label: String? = nil,
-                            height pixels: Int? = nil,
-                            theme: ColorTheme? = nil,
-                            isStriped: Bool = false,
-                            isAnimated: Bool = false,
-                            @TagBuilder contents: () -> [Tag]) {
+    public convenience init(label: String, height pixels: Int? = nil) {
         let div = Div {
             if let label = label {
                 Text(label)
             }
-            contents()
         }
-        self.init(percent: percent,
-                  height: pixels,
-                  theme: theme,
-                  isStriped: isStriped,
-                  isAnimated: isAnimated,
-                  div)
+        self.init(height: pixels, div)
     }
     
-    public init(percent: Int,
-                height pixels: Int?,
-                theme: ColorTheme?,
-                isStriped: Bool,
-                isAnimated: Bool,
-                _ div: Div) {
-        let percent = percent <= 100 ? percent : 100
+    public init(height pixels: Int?, _ div: Div) {
         let pixels = pixels != nil ? "\(pixels!)" : nil
         div
             .class(insert: .progressbar)
             .role(.progressbar)
-            .style(set: .width("\(percent)%"))
-            .ariaValuenow("\(percent)")
             .ariaValuemin("0")
             .ariaValuemax("100")
-            .class(insert: theme?.backgroundClass)
             .style(set: .height(pixels))
         
         super.init(div)
     }
+    
+    @discardableResult
+    public func percent(_ value: Double, _ condition: Bool = true) -> Self {
+        guard condition else { return self }
+        let percent = value <= 100 ? value : 100
+        tag
+            .style(set: .width("\(percent)%"))
+            .ariaValuenow("\(percent)")
+        return self
+    }
+    
+    @discardableResult
+    public func isStriped(if condition: Bool = true) -> Self {
+        guard condition else { return self }
+        tag
+            .class(insert: .progressBarStriped)
+        return self
+    }
+    
+    @discardableResult
+    public func isAnimated(if condition: Bool = true) -> Self {
+        guard condition else { return self }
+        tag
+            .class(insert: .progressBarAnimated)
+        return self
+    }
 }
-
