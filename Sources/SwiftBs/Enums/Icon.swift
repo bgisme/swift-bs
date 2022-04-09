42f0a38377c931aff4a20cf502516789c4eef40a
bgisme <bg4all@me.com>
2022-04-09 07:42:27 -0400

    icons

---
+++ Sources/SwiftBs/Enums/Icon.swift
@@ -0,0 +1,23 @@
+//
+//  Icon.swift
+//  
+//
+//  Created by Brad Gourley on 4/9/22.
+//
+
+public enum Icon: String {
+    case biAlarm = "bi-alarm"
+    case biExclamationCircle = "bi-exclamation-circle"
+    case biCalendar3 = "bi-calendar3"
+    case biPlayCircle = "bi-play-circle"
+    case biCurrencyDollar = "bi-currency-dollar"
+    case biCashCoin = "bi-cash-coin"
+    case biCoin = "bi-coin"
+    case biChatQuote = "bi-chat-quote"
+    case biChatLeftDots = "bi-chat-left-dots"
+    case biCart3 = "bi-cart3"
+    case biEye = "bi-eye"
+    case biEmojiHeartEyes = "bi-emoji-heart-eyes"
+    case biCheck2Square = "bi-check2-square"
+    case biHandIndexThumb = "bi-hand-index-thumb"
+}
